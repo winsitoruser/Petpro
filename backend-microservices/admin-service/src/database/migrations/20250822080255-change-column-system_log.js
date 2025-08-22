4d6b66ba8c6a9527c59636cdd4ad37ef43ab6f19
handrawanwdev <handrawanw.dev@gmail.com>
2025-08-22 15:54:18 +0700

    fix(migration): streamline updated_at column addition and removal logic

--- backend-microservices/admin-service/src/database/migrations/20250822080255-change-column-system_log.js
+++ backend-microservices/admin-service/src/database/migrations/20250822080255-change-column-system_log.js
@@ -1,29 +1,24 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    const table = await queryInterface.describeTable("system_logs");
 
-    await queryInterface.addColumn('system_logs', 'updated_at', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.fn('NOW'),
-    });
+    if (!table.updated_at) {
+      await queryInterface.addColumn("system_logs", "updated_at", {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      });
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.removeColumn('system_logs', 'updated_at');
-  }
+  async down(queryInterface, Sequelize) {
+    const table = await queryInterface.describeTable("system_logs");
+
+    if (table.updated_at) {
+      await queryInterface.removeColumn("system_logs", "updated_at");
+    }
+  },
 };
